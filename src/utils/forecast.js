5a698f913247dc104f7e2b6a14ce56f2e8025edc
flandalanda <flandalanda@gmail.com>
2022-02-16 20:10:04 -0600

    Added chance of precipitation to forecastData

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback("Unable to find location", undefined)
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]} It is currently ${body.current.temperature} degrees. However, it feels like ${body.current.feelslike}`)
+            const chanceOfPrecip = body.current.precip * 100
+            callback(undefined, `It is currently ${body.current.weather_descriptions[0]} with a temperature of ${body.current.temperature} degrees. However, it feels like ${body.current.feelslike} degrees. The chance of precipitation is of ${chanceOfPrecip}%.`)
         }
     
     })
